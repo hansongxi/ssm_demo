1f41152536c0e25e1ef6387a04c081605c226332
hansongxi <18634810449@163.com>
2018-12-27 21:27:29 +0800

    日志的添加

--- src/main/java/com/wntiem/ssm_web/controller/PersonController.java
+++ src/main/java/com/wntiem/ssm_web/controller/PersonController.java
@@ -2,6 +2,8 @@ package com.wntiem.ssm_web.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,11 @@ import com.wntiem.ssm_web.service.PersonService;
 
 @Controller
 public class PersonController {
+	
+	
+	
+
+	public static final Logger logger = LoggerFactory.getLogger(PersonController.class);
 
 	@Autowired
 	PersonService personService;
@@ -19,6 +26,13 @@ public class PersonController {
 	@RequestMapping("/showPerson")
 	public String showPersons(Model model) {
 		List<Person> persons = personService.loadPersons();
+		System.out.println("Sssssss");
+		System.out.println(logger.getName());
+		logger.info("chnggogn ddddddddddd");
+		if(persons!=null) {
+			
+			logger.info("chnddd");
+		}
 		model.addAttribute("persons", persons);
 		return "showperson";
 	}
